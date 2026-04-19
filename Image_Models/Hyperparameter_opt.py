1fbd9ccab9983aeb05595ba26d08f95c82b1e530
EBauer-27 <evabauer@web.de>
2026-04-19 13:28:06 +0200

    Updates code

--- Image_Models/Hyperparameter_opt.py
+++ Image_Models/Hyperparameter_opt.py
@@ -7,10 +7,13 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.tensorboard.summary import hparams
 from torchvision import transforms
+import optuna
 
 from data_processing.data import MIDASDataset
 from Image_Models.Models import ResNet18, VggNet, GoogLeNet
 from Image_Models.train import Trainer
+
+
 
 ## CHECK OPTUNA !!!!!
 
@@ -41,16 +44,10 @@ def data_loaders(train_path, val_path, img_root, train_tf, val_tf, batch_size):
 
     return train_loader, val_loader 
 
+
 IMG_ROOT = 'data/MRA-MIDAS/midas_224_cache/'
 TRAIN_PATH = 'manifests/train.csv'
 VAL_PATH   = 'manifests/val.csv'
-EPOCHS = 100
-
-search_space = {'model': ['resnet', 'googlenet', 'vggnet'],
-                'lr': [1e-2, 1e-3, 1e-4],
-                'batch_size': [32, 64],
-                'weight_decay': [1e-4, 1e-5],
-                }
 
 train_transforms = transforms.Compose([
     transforms.RandomHorizontalFlip(),
@@ -64,24 +61,18 @@ val_transforms = transforms.Compose([
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), # adjust mean/std if needed (currently set to [0.5] for grayscale images; change to ImageNet stats if using pretrained models on RGB)
 ])
 
+def objective(trial):
+    model_name = trial.suggest_categorical('model_name', ['resnet', 'googlenet', 'vggnet'])
+    lr = trial.suggest_float('lr', 1e-5, 1e-3, log=True)
+    batch_size = trial.suggest_categorical('batch_size', [32, 64])
+    weight_decay = trial.suggest_float('weight_decay', 1e-6, 1e-3, log=True)
 
-keys = list(search_space.keys())
-values = list(search_space.values())
+    epochs = 50
 
-base_logdir = "runs/hparam_search"
-os.makedirs(base_logdir, exist_ok=True)
+    train_loader, val_loader = data_loaders(TRAIN_PATH, VAL_PATH, IMG_ROOT, train_transforms, val_transforms, batch_size=batch_size)
+    model = build_model(model_name)
 
-best_result = None
-best_score = -1
-
-for run_id, combo in enumerate(product(*values)):
-    config = dict(zip(keys, combo))
-    print(f'Run {run_id}: {config}')
-
-    train_loader, val_loader = data_loaders(TRAIN_PATH, VAL_PATH, IMG_ROOT, train_transforms, val_transforms, batch_size=config['batch_size'])
-    model = build_model(config['model'])
-
-    optimizer = torch.optim.AdamW(model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
     criterion = nn.BCEWithLogitsLoss()
 
     trainer = Trainer(
@@ -90,62 +81,53 @@ for run_id, combo in enumerate(product(*values)):
             valid_loader=val_loader,
             criterion=criterion,
             optimizer=optimizer,
-            end_epoch=EPOCHS
+            end_epoch=epochs,
+            trail=trial.number
         )
     
-    train_losses, val_losses = trainer.fit()
+    history = trainer.fit(trial=trial)
 
-    final_train_loss = train_losses[-1]
-    final_val_loss = val_losses[-1] if len(val_losses) > 0 else None
+    best_val_auc = max(history['val_auc'])
+    trial.set_user_attr("best_val_loss", min(history["val_loss"]))
+    trial.set_user_attr("final_val_auc", history["val_auc"][-1])
+    trial.set_user_attr("final_val_f1", history["val_f1"][-1])
+    trial.set_user_attr("final_val_acc", history["val_acc"][-1])
+    trial.set_user_attr("final_val_pres", history["val_pres"][-1])
 
-    run_name = (
-        f"run_{run_id}_"
-        f"{config['model']}_"
-        f"lr{config['lr']}_"
-        f"bs{config['batch_size']}_"
-        f"wd{config['weight_decay']}"
-    )
+    os.makedirs('Image_Models/checkpoints_opt', exist_ok=True)
+    best_model_path = f"Image_Models/checkpoints_opt/trial_{trial.number}.pt"
+    torch.save(model.state_dict(), best_model_path)
+    trial.set_user_attr("model_path", best_model_path)
 
-    writer = SummaryWriter(log_dir=os.path.join(base_logdir, run_name))
+    return best_val_auc
 
-    for k, v in config.items():
-        if isinstance(v, str):
-            writer.add_text(f"hparams/{k}", str(v))
-        else:
-            writer.add_scalar(f"hparams/{k}", v, 0)
+if __name__ == '__main__':
+    os.makedirs('Image_Models/optuna_db', exist_ok=True)
 
-    for epoch, loss in enumerate(train_losses):
-        writer.add_scalar("loss/train", loss, epoch)
+    study = optuna.create_study(study_name='img_models',
+                                storage='sqlite:///Image_Models/optuna_db/image_model_hyperopt_final.db',
+                                load_if_exists=True,
+                                direction='maximize',
+                                pruner=optuna.pruners.MedianPruner(n_startup_trials=5,
+                                                                   n_warmup_steps=5),
+                                )
+    
+    #study.optimize(objective, n_trials=20, show_progress_bar=False)
 
-    for epoch, loss in enumerate(val_losses):
-        writer.add_scalar("loss/val", loss, epoch)
+    try:
+        study.optimize(objective, n_trials=20)
+    except Exception as e:
+        print("TOP LEVEL:", repr(e))
+        print("CAUSE:", repr(getattr(e, "__cause__", None)))
+        raise
 
-    metric_dict = {
-        "hparam/final_train_loss": final_train_loss,
-        "hparam/final_val_loss": final_val_loss if final_val_loss is not None else float("nan"),
-    }
-
-    exp, ssi, sei = hparams(config, metric_dict)
-    writer.file_writer.add_summary(exp)
-    writer.file_writer.add_summary(ssi)
-    writer.file_writer.add_summary(sei)
-
-    writer.close()
-
-    if final_val_loss is not None:
-            score = -final_val_loss
-            if score > best_score:
-                best_score = score
-                best_result = {
-                    "config": config,
-                    "final_train_loss": final_train_loss,
-                    "final_val_loss": final_val_loss,
-                }
-
-    print("\nBest result:")
-    print(best_result)
+    print("\nBest trial:")
+    print("Value (best val_auc):", study.best_trial.value)
+    print("Params:")
+    for k, v in study.best_trial.params.items():
+        print(f"  {k}: {v}")
 
 
-
+# Test also on F1, Precision, Sensitivity, Accuracy, ... 
 
 
--- Image_Models/train.py
+++ Image_Models/train.py
@@ -4,15 +4,16 @@ import os
 import torch
 import numpy as np
 from tqdm import tqdm
+from sklearn.metrics import roc_auc_score, f1_score, accuracy_score, precision_score
+import optuna
 
 import shutil
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 
 class Trainer:
-    def __init__(self, model, train_loader, valid_loader, criterion, optimizer, end_epoch, save=False):
+    def __init__(self, model, train_loader, valid_loader, criterion, optimizer, end_epoch, trail='test', save=False):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        print(self.device)
         self.model = model.to(self.device)
 
         self.optimizer = optimizer
@@ -29,6 +30,13 @@ class Trainer:
             os.mkdir('models_saved', exist_ok=True)
             name = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_")
             self.save_path = os.path.join('models_saved', name)
+
+        TBOARD = f'Image_Models/Tensorboard/Hyperopt/trial_{trail}'
+        os.makedirs(TBOARD)
+        
+
+        shutil.rmtree(TBOARD)
+        self.writer = SummaryWriter(TBOARD)
 
 
     def train_step(self):
@@ -56,6 +64,9 @@ class Trainer:
         self.model.eval()
 
         loss_list = []
+        all_labels = []
+        all_probs = []
+
         for batch in self.valid_loader:
             img = batch['image']
             img = img.to(self.device)
@@ -67,23 +78,69 @@ class Trainer:
             loss = self.criterion(pred, label)
             loss_list.append(loss.item())
 
-        return round(np.mean(loss_list), 5)
+            probs = torch.sigmoid(pred)
+            all_probs.extend(probs.cpu().numpy().tolist())
+            all_labels.extend(label.cpu().numpy().tolist())
+
+        val_loss = float(np.mean(loss_list))
+        
+        all_labels = np.array(all_labels).astype(int)
+        all_probs = np.array(all_probs)
+
+        all_preds = (all_probs >= 0.5).astype(int)
+        
+        val_auc = float(roc_auc_score(all_labels, all_probs))
+        val_f1 = float(f1_score(all_labels, all_preds))
+        val_acc = float(accuracy_score(all_labels, all_preds))
+        val_pres = float(precision_score(all_labels, all_preds))
+
+        return val_loss, val_auc, val_f1, val_acc, val_pres
     
-    def fit(self):
+    def fit(self, trial):
         progress = tqdm(range(self.end_epoch))
 
-        train_loss = []
-        val_loss = []
+        history = {
+            'train_loss': [],
+            'val_loss': [],
+            'val_auc': [],
+            'val_f1': [],
+            'val_acc': [],
+            'val_pres': []
+        }
 
         for epoch in progress:
-            loss = self.train_step()
-            train_loss.append(loss)
+            train_loss = self.train_step()
+            self.writer.add_scalar('Train Loss', train_loss, global_step=epoch)
+            
+            history['train_loss'].append(train_loss)
 
-            progress.set_description(f'Epoch: {epoch}/{self.end_epoch}, Train Loss:{loss}')
+            progress.set_description(f'Epoch: {epoch}/{self.end_epoch}, Train Loss:{train_loss}')
         
-            if (epoch+1)%10 == 0:
-                loss = self.valid_step(epoch)
-                val_loss.append(loss)
+            if (epoch)%10 == 0:
+                val_loss, val_auc, val_f1, val_acc, val_pres = self.valid_step(epoch)
+                self.writer.add_scalar('Val Loss', val_loss, global_step=epoch)
+                self.writer.add_scalar('Val F1', val_f1, global_step=epoch)
+                self.writer.add_scalar('Val ACC', val_acc, global_step=epoch)
+                self.writer.add_scalar('Val pres', val_pres, global_step=epoch)
+
+                history['val_loss'].append(val_loss)
+                history['val_auc'].append(val_auc)
+                history['val_f1'].append(val_f1)
+                history['val_acc'].append(val_acc)
+                history['val_pres'].append(val_pres)
+
+        val_loss, val_auc, val_f1, val_acc, val_pres = self.valid_step(epoch)
+        history['val_loss'].append(val_loss)
+        history['val_auc'].append(val_auc)
+        history['val_f1'].append(val_f1)
+        history['val_acc'].append(val_acc)
+        history['val_pres'].append(val_pres)
+
+        
+        if trial is not None:
+            trial.report(val_auc, step=epoch)
+            if trial.should_prune():
+                raise optuna.TrialPruned()
 
         if self.save:
             torch.save({'epoch': epoch,
@@ -92,6 +149,8 @@ class Trainer:
 
             print('Model saved!')
             
-        return train_loss, val_loss
+        self.writer.close()
+
+        return history
 
         
